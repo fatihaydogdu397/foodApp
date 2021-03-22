c1647c7f309db5d3cf8594727361b2dfa71f2799
fatih <45509775+fatihaydogdu397@users.noreply.github.com>
2021-03-22 13:54:48 +0300

    changes1

--- lib/views/list.dart
+++ lib/views/list.dart
@@ -1,5 +1,6 @@
 import 'package:auto_size_text/auto_size_text.dart';
 import 'package:flutter/material.dart';
+import 'package:food_app/core/widgets/list_widgets/food_list_item.dart';
 import 'package:responsive_grid/responsive_grid.dart';
 
 class FoodList extends StatelessWidget {
@@ -19,7 +20,8 @@ class FoodList extends StatelessWidget {
                 mainAxisAlignment: MainAxisAlignment.spaceBetween,
                 children: [
                   Icon(Icons.arrow_back_ios, color: Colors.grey, size: 30),
-                  Text("Fruits", style: TextStyle(fontSize: 28,color: Colors.grey[800])),
+                  Text("Fruits",
+                      style: TextStyle(fontSize: 28, color: Colors.grey[800])),
                   Container()
                 ],
               ),
@@ -31,134 +33,83 @@ class FoodList extends StatelessWidget {
                 children: [
                   Row(
                     children: [
-                      Icon(Icons.filter_alt_sharp,size:30,color: Colors.grey,),
-                      Icon(Icons.filter_alt_sharp,size:30,color: Colors.grey,),
+                      Icon(
+                        Icons.filter_alt_sharp,
+                        size: 30,
+                        color: Colors.grey,
+                      ),
+                      Icon(
+                        Icons.filter_alt_sharp,
+                        size: 30,
+                        color: Colors.grey,
+                      ),
                     ],
                   ),
-                  Icon(Icons.filter_alt_sharp,size:30,color: Colors.grey,),
+                  Icon(
+                    Icons.filter_alt_sharp,
+                    size: 30,
+                    color: Colors.grey,
+                  ),
                 ],
               ),
             ),
             Expanded(
-              child: ResponsiveGridList(
-                  desiredItemWidth: 200,
-                  minSpacing: 20,
+              
+              child: Container(
+                padding: EdgeInsets.only(left:20,right: 20),
+                child: GridView.count(
+                  crossAxisCount: 2,
+                  // shrinkWrap: true,
+                  crossAxisSpacing: 15,
+                  mainAxisSpacing: 15,
+                  primary: false,
+                  scrollDirection: Axis.vertical,
                   children: [
-                    RecommendedItem(),
-                    RecommendedItem(),
-                    RecommendedItem(),
-                    RecommendedItem(),
-                    RecommendedItem(),
-                    RecommendedItem(),
-                    RecommendedItem(),
-                    RecommendedItem(),
-                    RecommendedItem(),
-                    RecommendedItem(),
-                    RecommendedItem(),
-                    RecommendedItem(),
-                    RecommendedItem(),
-                  ]),
-            )
+                    FoodListItem(),
+                    FoodListItem(),
+                    FoodListItem(),
+                    FoodListItem(),
+                    FoodListItem(),
+                    FoodListItem(),
+                    FoodListItem(),
+                    FoodListItem(),
+                    FoodListItem(),
+                    FoodListItem(),
+                    FoodListItem(),
+                    FoodListItem(),
+                  ],
+                ),
+              ),
+            ),
+            // Expanded(
+            //   child: ResponsiveGridList(
+            //       desiredItemWidth: 200,
+            //       rowMainAxisAlignment: MainAxisAlignment.spaceBetween,
+            //       squareCells: true,
+            //       minSpacing: 20,
+            //       children: [
+            //         FoodListItem(),
+            //         FoodListItem(),
+            //         FoodListItem(),
+            //         FoodListItem(),
+            //         FoodListItem(),
+            //         FoodListItem(),
+            //         FoodListItem(),
+            //         FoodListItem(),
+            //         FoodListItem(),
+            //         FoodListItem(),
+            //         FoodListItem(),
+            //         FoodListItem(),
+            //         FoodListItem(),
+            //         FoodListItem(),
+            //         FoodListItem(),
+            //         FoodListItem(),
+            //         FoodListItem(),
+            //       ]),
+            // )
           ],
         ),
       ),
     ));
-  }
-}
-
-class RecommendedItem extends StatelessWidget {
-  const RecommendedItem({Key key}) : super(key: key);
-
-  @override
-  Widget build(BuildContext context) {
-    return Container(
-      width: 200,
-      height: 200,
-      // color: Colors.black,
-      // padding: EdgeInsets.only(right: 20, top: 10),
-      child: Stack(
-        children: [
-          ClipRRect(
-            borderRadius: BorderRadius.circular(12),
-            child: Image(
-                height: 200,
-                fit: BoxFit.cover,
-                image: NetworkImage(
-                    "https://thelatinahomemaker.com/wp-content/uploads/2017/02/fruit-salad-3.png")),
-          ),
-          Positioned(
-            right: 8,
-            top: 8,
-            child: ClipOval(
-              child: Container(
-                  decoration: BoxDecoration(
-                      // borderRadius: BorderRadius.circular(12),
-                      color: Colors.black.withOpacity(0.4)),
-                  child: Padding(
-                    padding: const EdgeInsets.all(3.0),
-                    child: Icon(
-                      Icons.bookmark_border,
-                      color: Colors.white,
-                      size: 20,
-                    ),
-                  )),
-            ),
-          ),
-          Positioned(
-              left: 8,
-              right: 8,
-              bottom: 8,
-              child: Container(
-                padding: EdgeInsets.all(3),
-                decoration: BoxDecoration(
-                    borderRadius: BorderRadius.circular(12),
-                    color: Colors.black.withOpacity(0.4)),
-                child: Column(
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: [
-                    AutoSizeText(
-                      "Apple Milkshake",
-                      textAlign: TextAlign.left,
-                      style: TextStyle(
-                          fontWeight: FontWeight.w600, color: Colors.white),
-                    ),
-                    Container(
-                      padding: EdgeInsets.all(5),
-                      child: Row(
-                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                        crossAxisAlignment: CrossAxisAlignment.center,
-                        children: [
-                          Row(
-                            children: [
-                              Icon(
-                                Icons.star,
-                                color: Colors.yellow,
-                                size: 20,
-                              ),
-                              Text(
-                                "4.5",
-                                style: TextStyle(color: Colors.white),
-                              ),
-                            ],
-                          ),
-                          Row(
-                            children: [
-                              Icon(Icons.access_time,
-                                  size: 20, color: Colors.white),
-                              Text(
-                                "20 min",
-                                style: TextStyle(color: Colors.white),
-                              ),
-                            ],
-                          )
-                        ],
-                      ),
-                    ),
-                  ],
-                ),
-              ))
-        ],
-      ),
-    );
   }
 }
